8cbf50142daccab12e11033187c9afb31c6a2cf5
Judy <850832627@qq.com>
2018-01-07 00:01:04 +0800

    Update render.js

--- src/js/login/render.js
+++ src/js/login/render.js
@@ -1,5 +1,29 @@
 const template = (opts = {}) => {
-  return "<div>Hello</div>"
+  // class define style, id define selectors  For better maintainance
+  const tpl = `
+    <div class="login-wrapper">
+      <form id="login-form" onsubmit="return false">
+        <label class="login-account-wrapper">
+          <span class="account-label"> ${opts.accountLabel} </span>
+          <input
+            id="login-account"
+            type="text"
+            name="account"
+            placeholder="${opts.accountPlaceholder}">
+        </label>
+        <label class="login-account-wrapper">
+          <span class="account-label"> ${opts.passwordLabel} </span>
+          <input
+            id="login-account"
+            type="text"
+            name="account"
+            placeholder="${opts.passwordPlaceholder}">
+        </label>
+        <input id="login-btn" class="login-btn" type="submit" value="${opts.loginBtnText}"/>
+      </form>
+    </div>
+  `
+  return tpl
 }
 
 export default (conf = {}) => {
