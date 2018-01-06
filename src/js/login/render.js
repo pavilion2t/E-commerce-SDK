ac228b3796f2fca4b0a901104a10f463bd04b6af
Judy <850832627@qq.com>
2018-01-07 00:10:06 +0800

    Update render.js

--- src/js/login/render.js
+++ src/js/login/render.js
@@ -1,15 +1,25 @@
 const template = (opts = {}) => {
+  // tips: delete autocomplete
+  const autocompleteTpl = `
+  <div>
+    <input type="text"/>
+    <input type="password"/>
+  </div>`;
+  const autocompleteAdapter = opts.autocomplete ? "" :  autocompleteTpl;
+  const autocompleteValue = opts.autocomplete ? "on" : "off";
   // class define style, id define selectors  For better maintainance
   const tpl = `
     <div class="login-wrapper">
       <form id="login-form" onsubmit="return false">
+        ${ autocompleteAdapter}
         <label class="login-account-wrapper">
           <span class="account-label"> ${opts.accountLabel} </span>
           <input
             id="login-account"
             type="text"
             name="account"
-            placeholder="${opts.accountPlaceholder}">
+            placeholder="${opts.accountPlaceholder}"
+            autocomplete= "${ autocompleteValue }">
         </label>
         <label class="login-account-wrapper">
           <span class="account-label"> ${opts.passwordLabel} </span>
@@ -17,7 +27,8 @@ const template = (opts = {}) => {
             id="login-account"
             type="text"
             name="account"
-            placeholder="${opts.passwordPlaceholder}">
+            placeholder="${opts.passwordPlaceholder}"
+            autocomplete= "${ autocompleteValue }">
         </label>
         <input id="login-btn" class="login-btn" type="submit" value="${opts.loginBtnText}"/>
       </form>
