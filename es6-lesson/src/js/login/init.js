8c0a37ebacbd5458c3c81b7484eaa5194afdb81b
Judy <judy.zhu@bindo.com>
2018-01-15 00:07:40 +0800

    登录源码文件替换

--- es6-lesson/src/js/login/init.js
+++ es6-lesson/src/js/login/init.js
@@ -1,23 +1,29 @@
-import "../common/polyfill";
-import render from "./render";
-import bindEvent from "./event";
+import '../common/polyfill';
+import render from './render';
+import bindEvent from './event';
+
+
 
 const login = (opts = {}) => {
-  var defaultOpts = {
-    loginBtnText: "登录",
-    accountPlaceHolder: "",
-    accountLabel: "",
-    passwordPlaceHolder: "",
-    passwordLabel: "",
-    verifyPlaceHolder: "",
-    accountMax: "30",
-    passwordMax: "30",
-    showRemember: true,
-    autocomplete: false,
-  };
-  var options = Object.assign(defaultOpts, opts);
-  render(options);
-  bindEvent(options);
+    var defaultOpts = {
+        loginBtnText: '登 录',
+        accountPlaceHolder: '手机号/邮箱/账号',
+        accountLabel: '',
+        passwordPlaceHolder: '请填写密码',
+        passwordLabel: '',
+        verifyPlaceHolder: '验证码',
+        accountMax: '30',
+        passwordMax: '30',
+        showRemember: true,
+        autocomplete: false,
+    };
+
+    var options = Object.assign(defaultOpts, opts)
+
+    render(options);
+    bindEvent(options);
 }
 
 export { login }
+
+// new FormCheck({});
